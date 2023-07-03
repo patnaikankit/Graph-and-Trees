0186bfe7d93daf9ca5fa01aec82bbe267ce56cd9
Ankit Patnaik <116827173+patnaikankit@users.noreply.github.com>
2023-07-03 16:47:46 +0530

    bipartite

--- Graph/bfs.cpp
+++ Graph/bfs.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 const int N = 1e5 + 10;
-vector<int> g[N];
+vector<int> graph[N];
 int vis[N];
 int level[N];
 
@@ -9,11 +9,11 @@ void bfs(int source){
     queue<int> q;
     q.push(source);
     vis[source] = 1;
-    while (!q.empty()){
+    while(!q.empty()){
         int cur_v = q.front();
         q.pop();
         cout<<cur_v<<" ";
-        for(int child: g[cur_v]){
+        for(int child: graph[cur_v]){
             if(!vis[child]){
                 q.push(child);
                 vis[child] = 1;
@@ -30,8 +30,8 @@ int main(){
     for(int i = 0; i < m; i++){
         int u,v;
         cin>>u>>v;
-        g[u].push_back(v);
-        g[v].push_back(u);
+        graph[u].push_back(v);
+        graph[v].push_back(u);
     }
     bfs(1);
     for(int i = 1; i <= n; i++){
---
+++ Graph/bipartite.cpp
@@ -0,0 +1,40 @@
+// if you take 2 colours and start colouring the nodes in such a way that no 2 adjacent nodes have the same colour 
+// a linear graph with no cycles always and a graph containing an even length cycle are always bipartite
+#include <bits/stdc++.h>
+using namespace std;
+const int N = 1e5 + 10;
+vector<int> graph[N];
+vector<int> colour(N, -1);
+
+bool bipartite(int node, vector<int> graph[]){
+    queue<int> q;
+    q.push(node);
+    colour[node] = 0;
+    while(!q.empty()){
+        int cur_v = q.front();
+        q.pop();
+        for(auto child: graph[cur_v]){
+            if(colour[child] == -1 || colour[child] == colour[cur_v]){
+                return false;
+            }
+            else if(colour[child] == -1){
+                colour[child] = !colour[cur_v];
+                q.push(child);
+            }
+        }
+    }
+    return true;
+}
+
+int main(){
+    int n,m;
+    cin>>n>>m;
+    for(int i = 0; i < m; i++){
+        int u,v;
+        cin>>u>>v;
+        graph[u].push_back(v);
+        graph[v].push_back(u);
+    }
+    cout<<bipartite(1, graph)<<endl;
+    return 0;
+}
